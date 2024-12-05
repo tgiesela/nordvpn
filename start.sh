c667dccd91c9929b357feafecaba7e34dff431fe
Tonny Gieselaar <tgiesela@gmail.com>
2024-12-05 08:59:49 +0100

    Minor changes to avoid errors in bash scripts

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@
 function wait_for_vpn() {
     # Now wait for the VPN servive to become ready
     STATUS=$(docker ps --format '{{.Status}}' -f name=nordvpn)
-    while [[ ! "${STATUS}" =~ "(healthy)" ]] ; do
+    while [[ ! "${STATUS}" =~ (healthy) ]] ; do
         echo "Waiting for VPN to become ready"
         sleep 5
         STATUS=$(docker ps --format '{{.Status}}' -f name=nordvpn)
@@ -37,11 +37,12 @@ case "$CMD" in
        echo "Illegal start option, should be one of 'build, start', found ${CMD}"
        exit 1
 esac
+# shellcheck source=/dev/null
 source load_vars.sh
 
 if [ "$CMD" == "build" ] ; then
     # Note MTU 1420 is from nordvpn nordlynx interface
-    docker network create --subnet ${DOCKERNETWORK} --gateway ${DOCKERGATEWAY} --ipv6 mailnet --opt com.docker.network.driver.mtu=1420
+    docker network create --subnet "${DOCKERNETWORK}" --gateway "${DOCKERGATEWAY}" --ipv6 mailnet --opt com.docker.network.driver.mtu=1420
     docker compose --parallel 1 up --build -d vpn
     wait_for_vpn
     docker compose --parallel 1 up --build -d 
@@ -56,26 +57,26 @@ fi
 HIDDENCONTAINERS=$(docker ps --format '{{.Names}}' --filter "label=com.tgiesela.vpn.hiddenip=true")
 for container in $HIDDENCONTAINERS ; do
     echo -e "\nProcessing docker container: $container\n"
-    docker exec --privileged ${container} ip route show
-    docker exec --privileged ${container} ip route del default
-    docker exec --privileged ${container} ip route add default via ${VPNIP}
+    docker exec --privileged "${container}" ip route show
+    docker exec --privileged "${container}" ip route del default
+    docker exec --privileged "${container}" ip route add default via "${VPNIP}"
 
     IFS=';' read -ra ADDR <<< "$LOCALNETWORKS"
     for i in "${ADDR[@]}"; do
-       if [ $i != $DOCKERNETWORK ] ; then
-           docker exec --privileged ${container} ip route add $i via ${DOCKERGATEWAY}
+       if [ "$i" != "$DOCKERNETWORK" ] ; then
+           docker exec --privileged "${container}" ip route add "$i" via "${DOCKERGATEWAY}"
        fi
     done
 
-    docker exec --privileged ${container} ip route show
+    docker exec --privileged "${container}" ip route show
 done
 BEHINDVPNCONTAINERS=$(docker ps --format '{{.Names}}' --filter "label=com.tgiesela.vpn.accessible=true")
 for container in $BEHINDVPNCONTAINERS ; do
     echo -e "\nFORWARDING Processing docker container: $container\n"
-    SRCPORTS=$(docker inspect --format '{{ index .Config.Labels "com.tgiesela.vpn.vpnport"}}' ${container})
-    DSTPORTS=$(docker inspect --format '{{ index .Config.Labels "com.tgiesela.vpn.containerport"}}' ${container})
-    IFS=';' read -ra SRCPORTARRAY <<< $SRCPORTS
-    IFS=';' read -ra DSTPORTARRAY <<< $DSTPORTS
+    SRCPORTS=$(docker inspect --format '{{ index .Config.Labels "com.tgiesela.vpn.vpnport"}}' "${container}")
+    DSTPORTS=$(docker inspect --format '{{ index .Config.Labels "com.tgiesela.vpn.containerport"}}' "${container}")
+    IFS=';' read -ra SRCPORTARRAY <<< "$SRCPORTS"
+    IFS=';' read -ra DSTPORTARRAY <<< "$DSTPORTS"
     if [ ${#SRCPORTARRAY[@]} != ${#DSTPORTARRAY[@]} ] ; then
        echo 'Length of vpnport array does not match length of containerport array'
        exit 1
@@ -85,7 +86,7 @@ for container in $BEHINDVPNCONTAINERS ; do
     for ((i=0; i<${#SRCPORTARRAY[@]}; i++)); do
         SRCPORT=${SRCPORTARRAY[i]}
         DSTPORT=${DSTPORTARRAY[i]}
-        docker exec nordvpn /forward.sh ${SRCPORT} ${container} ${DSTPORT}
+        docker exec nordvpn /forward.sh "${SRCPORT}" "${container}" "${DSTPORT}"
     done
 done
 # Temporary solution until problem solved (iptables, fragmentation, MTU-size)
