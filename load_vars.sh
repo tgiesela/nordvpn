06d319416a5f76722399dbeb2cd9738cd58ae657
Tonny Gieselaar <tgiesela@gmail.com>
2024-12-05 08:44:42 +0100

    Update load_vars to avoid bash syntax checker issues

--- load_vars.sh
+++ load_vars.sh
@@ -1,11 +1,13 @@
-# ! /bin/bash
-for dir in $(ls -d ../*/ ./*/) ;do
-echo "Processing $dir"
+#!/bin/bash
+FOLDERS=$(find ../*/ -mindepth 0 -maxdepth 0 -type d && find ./*/ -mindepth 0 -maxdepth 0 -type d)
+for dir in ${FOLDERS} ; do
+   echo "Processing $dir"
    if [ -f "${dir}/docker-compose.yml" ] ; then
-echo "    $dir contains compose.yml"
-       if [ -f "${dir}/vars" ] ; then
-echo "    source vars from $dirvars"
-            source ${dir}vars
-       fi
+      echo "    $dir contains compose.yml"
+      if [ -f "${dir}/vars" ] ; then
+         echo "    source vars from ${dir}/vars"
+         # shellcheck source=/dev/null
+         source "${dir}"/vars
+      fi
    fi
 done
